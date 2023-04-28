f94d086870fc4b3b167a047acbc02623266b12b0
HochinChan <HochinChan@163.com>
2023-04-28 14:14:24 +0800

    没头绪，不知道要做什么

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 <template>
-  <h1></h1>
+  <RouterView />
 </template>
 <style scoped></style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 <template>
-  <div></div>
+  <div>home</div>
 </template>
 <style lang="less" scoped></style>
