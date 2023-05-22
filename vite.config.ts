7de2d5c36c0ff69964970a0a9863b49bd7b305c4
HochinChan <HochinChan@163.com>
2023-05-22 14:31:26 +0800

    配置按需自动导入组件和模块

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,26 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      eslintrc: { enabled: true },
+      dts: "types/auto-import.d.ts",
+      resolvers: [ElementPlusResolver()],
+      imports: ["vue", "vue-router", "pinia"],
+    }),
+    Components({
+      dirs: ["src/components"],
+      dts: "types/components.d.ts",
+      resolvers: [ElementPlusResolver({ importStyle: false })],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
